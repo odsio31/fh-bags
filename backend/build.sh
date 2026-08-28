da9c6e2045dc1ec84bf112b64d71433569e928af
sido <odisosido@gmail.com>
2026-08-28 22:48:09 +0100

    zadna suoer user

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python -c "import os, django; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings'); django.setup(); from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='sido').exists() or User.objects.create_superuser('fateh', 'fateh@gmail.com', 'fateh@123')"
